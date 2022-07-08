4295b8eacad6007b90aca08f5bdd13e7ae351bbf
MrVarma <varmasandeep933@gmail.com>
2022-07-08 22:58:30 +0530

    second commit

---
+++ Models/employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment5.Models
+{
+    [Table("Employee")]
+    public class employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string EName { get; set; }
+
+        public string EDesign { get; set; }
+
+        public DateTime? EDOJ { get; set; }
+    }
+}
